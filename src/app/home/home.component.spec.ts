bff565bdc7551549d0a231dda1841ffabd431061
Simon <gonzalezsimon18@gmail.com>
2017-08-07 00:56:58 -0430

    Licencia código

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
 
 import { HomeComponent } from './home.component';
 import { PostCardComponent } from '../post-card/post-card.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PostService } from '../shared/post.service';
 import { HttpModule } from '@angular/http';
+import { By } from '@angular/platform-browser'
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -28,4 +32,17 @@ describe('HomeComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('loader should be false when call api posts gets error', () => {
+    fixture.detectChanges();
+    let postService = fixture.debugElement.injector.get(PostService);
+    let spy = spyOn(postService, 'getPosts').and.returnValue({ subscribe: err => {} });
+    fixture.whenStable().then(() => { 
+      fixture.detectChanges();
+      const loader = component.loader;
+      expect(loader).toBe(false);
+    });
+    component.ngOnInit();
+  });
+
 });
